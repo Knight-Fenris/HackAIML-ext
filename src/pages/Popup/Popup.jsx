268cf219cde874e6f81f5eb5490222419197358b
= <ribhavaggarwal29@gmail.com>
2024-02-10 11:58:08 +0530

    init2

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
-import './Popup.css';
+import styled from 'styled-components'
 
 const Popup = () => {
+  const getCurrentTab = async () => {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    console.log(tab);
+    // return tab;
+    console.log("https://developer.chrome.com/docs/extensions/reference/api/tabs#method-get")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
+    // console.log()
+
+    <PopupStyled className="App">
+      <button className="button" onClick={getCurrentTab}>
+        Scan
+      </button>
+    </PopupStyled>
   );
 };
+
+const PopupStyled = styled.div`
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  text-align: center;
+  height: 100%;
+  padding: 10px;
+  background-color: #282c34;
+  
+`
 
 export default Popup;
